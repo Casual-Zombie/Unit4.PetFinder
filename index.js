d376275b9e8db333b05daf5b9eb6db87949fab61
Casual-Zombie <jordanbaran80@gmail.com>
2024-03-14 15:06:03 -0700

    Initial commit

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const pets = require('./data');
 
 // init express app
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = 8080;
 
+app.use(cors());
+
+
 // GET - / - returns homepage
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, filePath) => {
+        if (path.extname(filePath) === '.css') {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // hello world route
@@ -21,32 +35,35 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const { owner } = req.query; 
 
     // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+    const ownerPets = pets.filter(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(ownerPets);
 });
+
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
-
+    // get the name from the request params
+    const name = req.params.name;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet);
 });
+
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
